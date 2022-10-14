a7c9fd3ae3094795695945ad3a3e3c73f8b67037
zengfei <zhengfei0311@gmail.com>
2022-10-14 09:39:08 +0800

    first commit

---
+++ build/CMakeFiles/zhengfei.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zhengfei.dir/main.cpp.o"
+  "zhengfei"
+  "zhengfei.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zhengfei.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
